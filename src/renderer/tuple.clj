344ab3e6801e3196a37040fe669de8adc805e591
arolet <antoine.rolet@gmail.com>
2023-01-28 16:14:11 +0900

    :zap: add vector functions

--- src/renderer/tuple.clj
+++ src/renderer/tuple.clj
@@ -10,4 +10,56 @@
 
 (defn isPoint [tuple] (== (:w tuple) 1.0))
 
-(defn add [a b] (->Tuple (+ (:x a) (:x b)) (+ (:y a) (:y b)) (+ (:z a) (:z b)) (- (+ (:w a) (:w b)) (* (:w a) (:w b)))))
+(defn equal [a b]
+  (reduce (fn [ncoll v]
+            (and ncoll (== (v a) (v b))))
+          true
+          [:x :y :z :w]
+          ))
+
+(defn add [a b]
+  (def merged (merge-with + a b))
+  (assoc merged :w (min 1 (:w merged)))
+  )
+
+(defn removeTuple [a b] (merge-with - a b))
+
+(defn mul [a factor]
+  (def merged (reduce (fn [ncoll [k v]]
+                        (assoc ncoll k (* factor v)))
+                      {}
+                      a))
+  (assoc merged :w (:w a))
+  )
+
+(defn div [a factor]
+  (mul a (/ 1. factor)))
+
+(defn minus [a]
+  (mul a -1))
+
+(defn dot [a b]
+  (reduce (fn [ncoll v]
+            (+ ncoll (* (v a) (v b))))
+          0
+          [:x :y :z :w]
+          ))
+
+(defn norm [a]
+  (Math/sqrt
+    (reduce (fn [ncoll v]
+              (def val (v a))
+              (+ ncoll (* val val)))
+            0
+            [:x :y :z]
+            )))
+
+(defn cross [a b]
+  (makeVector (- (* (:y a) (:z b)) (* (:z a) (:y b)))
+              (- (* (:z a) (:x b)) (* (:x a) (:z b)))
+              (- (* (:x a) (:y b)) (* (:y a) (:x b)))
+              )
+  )
+
+(defn normalize [a]
+  (div a (norm a)))
--- test/renderer/tuple_test.clj
+++ test/renderer/tuple_test.clj
@@ -27,16 +27,79 @@
               )
 
 (deftest testTupleEqual
-              (is (= (makeTestP) (makeTestP)))
-              (is (= (makeTestV) (makeTestV)))
-              (is (not (= (makeTestP) (makeTestV))))
-              (is (not (= (makeTestV) (makeTestP))))
-              (is (not (= (makePoint 4.0 -4.2 3.1) (makeTestP))))
-              (is (not (= (makePoint 4.3 -0.2 3.1) (makeTestP))))
-              (is (not (= (makePoint 4.3 -4.2 0.1) (makeTestP))))
+              (is (equal (makeTestP) (makeTestP)))
+              (is (equal (makeTestV) (makeTestV)))
+              (is (not (equal (makeTestP) (makeTestV))))
+              (is (not (equal (makeTestV) (makeTestP))))
+              (is (not (equal (makePoint 4.0 -4.2 3.1) (makeTestP))))
+              (is (not (equal (makePoint 4.3 -0.2 3.1) (makeTestP))))
+              (is (not (equal (makePoint 4.3 -4.2 0.1) (makeTestP))))
+              (is (equal (makePoint 0.0 -4.0 3.1) (makePoint 0 -4 3.1)))
+              (is (equal (makePoint 0.0 -4.0 3.1) (makePoint -0.0 -4.0 3.1)))
               )
 
 (deftest testTupleAdd
-              (is (= (makePoint 1 1 6) (add (makePoint 3 -2 5) (makeVector -2 3 1))))
-              (is (= (makePoint 1 1 6) (add (makePoint 3 -2 5) (makePoint -2 3 1))))
-              )
+  (is (equal (makePoint 1 1 6) (add (makePoint 3 -2 5) (makeVector -2 3 1))))
+  (is (equal (makePoint 1 1 6) (add (makePoint 3 -2 5) (makePoint -2 3 1))))
+  )
+
+(deftest testTupleRemove
+  (is (equal (makeVector 0 0 0) (removeTuple (makePoint 3 -2 5) (makePoint 3 -2 5))))
+  (is (equal (makePoint 0 0 0) (removeTuple (makePoint 3 -2 5) (makeVector 3 -2 5))))
+  (is (equal (makeVector 5 -5 4) (removeTuple (makePoint 3 -2 5) (makePoint -2 3 1))))
+  )
+
+(deftest testTupleMinus
+  (is (equal (makeVector -1 3 -2) (minus (makeVector 1 -3 2))))
+  (is (equal (makePoint -1 3 -2) (minus (makePoint 1 -3 2))))
+  )
+
+(deftest testTupleMul
+  (is (equal (makeVector 2 2 2) (mul (makeVector 1 1 1) 2)))
+  (is (equal (makePoint 5 -5 20) (mul (makePoint 2 -2 8) 2.5)))
+  (is (equal (makePoint -18 6 3) (mul (makePoint 6 -2 -1) -3)))
+  )
+
+(deftest testTupleDiv
+  (is (equal (makeVector 1 1 1) (div (makeVector -2 -2 -2) -2)))
+  (is (equal (makePoint 7 -4 2) (div (makePoint 56 -32 16) 8)))
+  (is (equal (makePoint 6 -2 -1) (div (makePoint -18 6 3) -3)))
+  )
+
+(deftest testDot
+  (is (== 0 (dot (makeVector 1 0 0) (makeVector 0 1 0))))
+  (is (== 0 (dot (makeVector 1 0 0) (makeVector 0 0 1))))
+  (is (== 0 (dot (makeVector 0 1 0) (makeVector 0 0 1))))
+  (is (== 1 (dot (makeVector 1 0 0) (makeVector 1 0 1))))
+  (is (== 1 (dot (makeVector 1 2 -2) (makeVector 1 1 1))))
+  (is (== 9 (dot (makeVector 1 2 -2) (makeVector 1 1 -3))))
+  )
+
+(deftest testCross
+  (is (equal (makeVector 0 0 1) (cross (makeVector 1 0 0) (makeVector 0 1 0))))
+  (is (equal (makeVector 0 0 -1) (cross (makeVector 0 1 0) (makeVector 1 0 0))))
+  (is (equal (makeVector 1 0 0) (cross (makeVector 0 1 0) (makeVector 0 0 1))))
+  (is (equal (makeVector -1 0 0) (cross (makeVector 0 0 1) (makeVector 0 1 0))))
+  (is (equal (makeVector 0 1 0) (cross (makeVector 0 0 1) (makeVector 1 0 0))))
+  (is (equal (makeVector 0 -1 0) (cross (makeVector 1 0 0) (makeVector 0 0 1))))
+  )
+
+(def sqrt3 (Math/sqrt 3))
+(deftest testNorm
+  (is (== 0 (norm (makeVector 0 0 0))))
+  (is (== 0 (norm (makePoint 0 0 0))))
+  (is (== 1 (norm (makeVector 1 0 0))))
+  (is (== 1 (norm (makeVector 0 1 0))))
+  (is (== 1 (norm (makeVector 0 0 1))))
+  (is (== sqrt3 (norm (makeVector 1 1 1))))
+  (is (== sqrt3 (norm (makePoint 1 1 1))))
+  )
+
+(def sqrt14_inv (/ 1 (Math/sqrt 14)))
+(def sqrt3Inv (/ 1 sqrt3))
+(deftest testNormalize
+  (is (equal (makeVector 1 0 0) (normalize (makeVector 10 0 0))))
+  (is (equal (makeVector -1 0 0) (normalize (makeVector -36 0 0))))
+  (is (equal (makeVector sqrt3Inv sqrt3Inv sqrt3Inv) (normalize (makeVector 1 1 1))))
+  (is (equal (makeVector sqrt14_inv (* 2 sqrt14_inv) (* 3 sqrt14_inv)) (normalize (makeVector 1 2 3))))
+  )
