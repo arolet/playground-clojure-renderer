6fa08a2bde321c3c5a2e1b41ed5b67965e8edba9
arolet <antoine.rolet@gmail.com>
2023-02-09 20:39:16 +0900

    :zap: add normal computation

--- src/renderer/objects.clj
+++ src/renderer/objects.clj
@@ -1,10 +1,13 @@
 (ns renderer.objects
-  (:require [renderer.matrix :refer [inverse mDotVec matEqual]]
+  (:require [renderer.matrix :refer [inverse mDotVec matEqual transpose]]
             [renderer.ray :as Ray]
+            [renderer.tuple :as Tuple]
             [renderer.transformation :refer
-             [chain rotationXYZ scaling sheering translation]]))
+             [chain rotationXYZ scaling sheering translation]]
+            [renderer.material :refer
+             [material]]))
 
-(defrecord Object3d [type intersect transformation inverted material])
+(defrecord Object3d [type intersect normalAt transformation inverted invertedTranspose material])
 
 (defn transformRay [mat {origin :origin direction :direction}]
   (Ray/makeRay (mDotVec mat origin) (mDotVec mat direction)))
@@ -30,7 +33,21 @@
   (matEqual (:transformation a) (:transformation b))
   )
 
+(declare normalAtUnitSphere)
+
+(defn makeSphereFromTransform [config transform]
+  (let [inverted (inverse transform)]
+    (->Object3d "sphere"
+                Ray/intersectUnitSphere
+                normalAtUnitSphere
+                transform
+                inverted
+                (transpose inverted)
+                config))
+  )
+
 (defn makeSphere
+  ([] (makeSphere (material)))
   ([config] (makeSphere config [0 0 0]))
   ([config position] (makeSphere config position [0 0 0]))
   ([config position rotate] (makeSphere config position rotate [1 1 1]))
@@ -40,6 +57,18 @@
                                (apply scaling scale)
                                (apply rotationXYZ rotate)
                                (apply translation position))]
-     (->Object3d "sphere" Ray/intersectUnitSphere transformation (inverse transformation) config))
+     (makeSphereFromTransform config
+                              transformation))
    )
+  )
+
+
+(def normalAtUnitSphere Tuple/castToVector)
+
+(defn normalAt [obj pt]
+  (let [ptOnUnitObj (mDotVec (:inverted obj) pt)
+        normalOnUnit ((:normalAt obj) ptOnUnitObj)
+        normalOnWorld (mDotVec (:invertedTranspose obj) normalOnUnit)
+        normalOnWorldFixed (Tuple/castToVector normalOnWorld)]
+    (Tuple/normalize normalOnWorldFixed))
   )
--- test/renderer/objects_test.clj
+++ test/renderer/objects_test.clj
@@ -1,10 +1,13 @@
 (ns renderer.objects_test
   (:require [clojure.test :refer :all]
             [renderer.matrix :refer [mDotVec makeIdentity matEqual]]
+            [renderer.transformation :refer [chain rotationZ scaling]]
             [renderer.objects :refer :all]
             [renderer.ray :refer [makeRay]]
             [renderer.tuple :refer [equal makePoint makeVector]]
-            [renderer.utils :refer [closeTo]]))
+            [renderer.utils :refer [closeTo]]
+            [renderer.material :refer
+             [material]]))
 
 (deftest testMakeSphere
   (is (= "sphere" (:type (makeSphere nil))))
@@ -29,5 +32,30 @@
         sphere (makeSphere nil [5 0 0])
         intersections (intersect ray [sphere])]
     (is (= 0 (count intersections)))
+    )
+  )
+
+(def sqrt2_2 (/ (Math/sqrt 2) 2))
+
+(deftest testNormalAtUnitSphere
+  (is (equal (makeVector 1 0 0) (normalAtUnitSphere (makePoint 1 0 0))))
+  (is (equal (makeVector 0 1 0) (normalAtUnitSphere (makePoint 0 1 0))))
+  (is (equal (makeVector 0 0 1) (normalAtUnitSphere (makePoint 0 0 1))))
+  (is (equal (makeVector sqrt2_2 sqrt2_2 0) (normalAtUnitSphere (makePoint sqrt2_2 sqrt2_2 0))))
+  )
+
+(deftest testNormalAtSphere
+  (is (equal (makeVector 0 sqrt2_2 (- sqrt2_2)) (normalAt (makeSphere nil [0 1 0]) (makePoint 0 (+ 1 sqrt2_2) (- sqrt2_2)))))
+  (is (equal (makeVector 0 0.97014 -0.24254)
+             (normalAt
+               (makeSphereFromTransform nil (chain (rotationZ (/ Math/PI 5))
+                                                   (scaling 1 0.5 1)))
+               (makePoint 0 sqrt2_2 (- sqrt2_2))) 1e-5))
+  )
+
+(deftest testSphereMaterial
+  (let [mater (material)
+        sphere (makeSphere mater)]
+    (is (= mater (:material sphere)))
     )
   )
